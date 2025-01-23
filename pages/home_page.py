76810338c21c689c85b7ff8a47fb78aa8a5feff2
Jeremy Marcos <jeremy@compassstarltd.com>
2025-01-23 22:52:46 +0800

    add logs

--- pages/home_page.py
+++ pages/home_page.py
@@ -11,4 +11,5 @@ class HomePage(BaseCase):
 
     def verify_home_page(self):
         self.sleep(2)
+        logger.info("Asserting the element")
         self.assert_element(self.welcome_label, timeout=20)
