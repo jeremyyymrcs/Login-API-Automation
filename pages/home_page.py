7963d29b04265c044ca2308749fea47dfba4ae72
Jeremy Marcos <jeremy@compassstarltd.com>
2025-01-24 18:16:02 +0800

    New Updates

--- pages/home_page.py
+++ pages/home_page.py
@@ -11,4 +11,5 @@ class HomePage(BaseCase):
 
     def verify_home_page(self):
         self.sleep(2)
+        self.assert_element(self.welcome_label)
         self.assert_url_contains("https://seleniumbase.io/realworld/")
