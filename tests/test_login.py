5ba2a48d8ab21c8245519b10e523054282726e6e
jeremyyymrcs <marcosjeremaya@gmail.com>
2025-01-26 23:23:17 +0800

    update the test order

--- tests/test_login.py
+++ tests/test_login.py
@@ -7,9 +7,11 @@ from pages.sign_up_page import SignUpPage
 class TestLogin(LoginPage, SignUpPage, HomePage):
 
     @pytest.mark.run(order=1)
-    def test_signup_and_login_with_totp_code(self):
+    def test_signup(self):
         self.get_secret_key()
-        self.switch_to_default_window()
+
+    @pytest.mark.run(order=1)
+    def test_login_with_totp_code(self):
         self.login_using_totp_code()
         self.verify_home_page()
 
