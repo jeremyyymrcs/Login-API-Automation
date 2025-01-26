8c952a5516ad904677fe0e6ac404ec216c88dc80
jeremyyymrcs <marcosjeremaya@gmail.com>
2025-01-26 22:23:24 +0800

    update totp_code locator

--- pages/sign_up_page.py
+++ pages/sign_up_page.py
@@ -6,7 +6,7 @@ logger = get_custom_logger(__name__)
 
 
 class SignUpPage(BaseCase):
-    toptp_code = '#totp'
+    toptp_code = '#totpcode'
     sign_up_redirection = "//a[contains(.,'seleniumbase.io/realworld/signup')]"
 
     def get_secret_key(self):
